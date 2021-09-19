903b5c96fc6a2dd2d76258f6c19db6170f0f7f3c
Rizwan Sarwar <71431169+Rizwan-Sarwar@users.noreply.github.com>
2021-09-19 20:57:25 +0500

    Update script.js
    Updated the Compute function

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function compute()
 {
-    debugger
     // p = document.getElementById("principal").value;
 
     var principal = document.getElementById("principal").value;
@@ -8,12 +7,20 @@ function compute()
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
+
+    if(principal <= 0){
+
+        alert("Please enter a positive numer.")
+
+    } else {
     
     document.getElementById("result").innerHTML=
     "If you deposit " + principal + 
     ",\<br\>at an interest rate of "+ rate +
     "%\<br\>You will receive an amount of "+ interest +
     ",\<br\>in the year "+ year +"\<br\>"
+
+}
     
 }
 function updateRate()
